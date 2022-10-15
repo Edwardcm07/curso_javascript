8625fd2688c719e4ae91902817d7cd0e1a4638ef
Edwardcm07 <ingeduarcanomontoya@gmail.com>
2022-10-15 14:27:00 -0500

    calculadora final

--- 8.Condicional_if/calculadora.js
+++ 8.Condicional_if/calculadora.js
@@ -1,15 +1,27 @@
 var num1 = prompt("introduce el primer número");
-var num2 = prompt("introduce el segundo número");
-var operacion = prompt(
-  "¿Qué operación deseas utilizar? suma/resta/multiplicación/división"
-);
 
-if (operacion == "suma") {
-  alert(parseInt(num1) + parseInt(num2));
-} else if (operacion == "resta") {
-  alert(parseInt(num1) - parseInt(num2));
-} else if (operacion == "multiplicación" || operacion == "multiplicacion") {
-  alert(parseInt(num1) * parseInt(num2));
-} else if (operacion == "división" || operacion == "division") {
-  alert(parseInt(num1) / parseInt(num2));
+var num2 = prompt("introduce el segundo número");
+
+if (!isNaN(num1) && !isNaN(num2)) {
+  var operacion = prompt(
+    "¿Qué operación deseas utilizar? suma/resta/multiplicación/división"
+  );
+
+  if (operacion == "suma") {
+    alert(parseInt(num1) + parseInt(num2));
+  } else if (operacion == "resta") {
+    alert(parseInt(num1) - parseInt(num2));
+  } else if (operacion == "multiplicación" || operacion == "multiplicacion") {
+    alert(parseInt(num1) * parseInt(num2));
+  } else if (operacion == "división" || operacion == "division") {
+    alert(parseInt(num1) / parseInt(num2));
+  } else {
+    alert("La operación no está contemplada");
+  }
+} else {
+  if (typeof num1 == "string" || typeof num1 == "string") {
+    alert("has introducido valores numéricos");
+  } else {
+    alert("ha ocurrido un error no identificado");
+  }
 }
